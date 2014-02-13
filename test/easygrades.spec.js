e1bd18cad3a25079c2612b60c0adf9f85f574210
SamuelRandall <samuelrandall999@gmail.com>
2014-02-12 22:42:35 -0600

    Added mocks Added easygrades test Added decoder

--- test/easygrades.spec.js
+++ test/easygrades.spec.js
@@ -3,7 +3,7 @@ var gradeDecoder = require("../lib/gradeDecoder");
 describe('easygrades', function(){
     describe('gradeDecoder', function(){
         it('it should return a decoded string', function(){
-            var encodedstring = '';
+            var encodedstring = "Zm9vYmFy";
             var decoder = new gradeDecoder();
             assert.equal('foobar', decoder.decode(encodedstring));
         })
