bec7e4c3ce169f4fd0677b28e45770f08bf41b13
SamuelRandall <samuelrandall999@gmail.com>
2014-02-23 19:29:05 -0600

    Moved mocks to test directory Added parentConnectionClient test Added nock to package.json

---
+++ test/parentConnectionClient.spec.js
@@ -0,0 +1,19 @@
+/**
+ * Created by samuel on 2/23/14.
+ */
+var nock = require('nock');
+var assert = require("assert");
+var parentConnectionClient = require("../lib/parentConnectionClient");
+describe('parentConnectionClient', function(){
+    describe('sessionID', function(){
+        it('should store session id after first GET request', function(){
+            var scope = nock('https://gradespeed.nisd.net')
+                .get('/pc/default.aspx?DistrictID=15915&timeout=1')
+                .replyWithFile(200, __dirname + '/mocks/getSessionIDResponse', {"Set-Cookie": "ASP.NET_SessionId=lsakedyxcp2rbywzlfs24h4k; path=/; HttpOnly"});
+            var client = new parentConnectionClient();
+            client.getSessionID();
+            assert.equal('foobar', client.sessionID);
+        })
+    })
+})
+
