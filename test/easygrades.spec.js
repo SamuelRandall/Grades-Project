caad2de6a904df0d3c72e360a4a76922c7814cb8
davi4797 <david.porter@rackspace.com>
2014-02-27 22:34:03 -0600

    Add cheerio library
    Add gradeTable.html mock file
    
    Add gradeDecoder.parseHtml method and test

--- test/easygrades.spec.js
+++ test/easygrades.spec.js
@@ -1,12 +1,40 @@
 var assert = require("assert");
 var gradeDecoder = require("../lib/gradeDecoder");
-describe('easygrades', function(){
-    describe('gradeDecoder', function(){
-        it('it should return a decoded string', function(){
-            var encodedstring = "Zm9vYmFy";
-            var decoder = new gradeDecoder();
-            assert.equal('foobar', decoder.decode(encodedstring));
-        })
-    })
-})
+var fs = require('fs');
+
+describe('easygrades', function () {
+  describe('gradeDecoder', function () {
+
+    it('it should return a decoded string', function () {
+      var encodedstring = "Zm9vYmFy";
+      var decoder = new gradeDecoder();
+      assert.equal(decoder.decode(encodedstring), 'foobar');
+    });
+
+    it('it should parse the HTML table and return an array of grades', function () {
+      var decoder = new gradeDecoder();
+      fs.readFile(__dirname + '/mocks/gradeTable.html', 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        var grades = decoder.parseHtml(data)
+        assert.equal(grades[0].teacher, 'Copeland, Joshua');
+        assert.equal(grades[0].course, 'US Hist AP D E');
+        assert.equal(grades[0].period, '01');
+        assert.equal(grades[0].cycle1, '97');
+        assert.equal(grades[0].cycle2, '93');
+        assert.equal(grades[0].cycle3, '95');
+        assert.equal(grades[0].exam1, '74');
+        assert.equal(grades[0].sem1, '91');
+        assert.equal(grades[0].cycle4, '92');
+        assert.equal(grades[0].cycle5, '');
+        assert.equal(grades[0].cycle6, '');
+        assert.equal(grades[0].exam2, '');
+        assert.equal(grades[0].sem2, '92');
+        assert.equal(grades.length, 9);
+      });
+    });
+
+  });
+});
 
