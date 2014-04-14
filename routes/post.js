8cde198e5c0de9a4bcc476aa913343b5523153ec
SamuelRandall <samuelrandall999@gmail.com>
2014-04-13 23:37:58 -0500

    Added live funtionality to the site so that users can imput their own login information

--- routes/post.js
+++ routes/post.js
@@ -10,49 +10,49 @@ var parentConnectionClient = require("../lib/parentConnectionClient");
 
 exports.post = function(req, res){
 
-//  var client = new parentConnectionClient();
-//  client.getSessionID(function(){
-//    client.login(req.body.username, req.body.password, function(){
-//      client.getGrades(function(){
-//        client.logout(function(){
-//          res.render('grades', { grades: client.grades });
-//        });
-//      });
-//    });
-//  });
-//
-  
-  var grades = [{
-    teacher: 'Copeland, Joshua',
-  course: 'US Hist AP D E',
-  period: '01',
-  cycle1: '97',
-  cycle2: '93',
-  cycle3: '95',
-  exam1: '74',
-  sem1: '91',
-  cycle4: '92',
-  cycle5: '93',
-  cycle6: '94',
-  exam2: '96',
-  sem2: '99'
-},
-    {
-      teacher: 'Copeland, Joshua',
-      course: 'US Hist AP D E',
-      period: '01',
-      cycle1: '97',
-      cycle2: '93',
-      cycle3: '95',
-      exam1: '74',
-      sem1: '91',
-      cycle4: '92',
-      cycle5: '93',
-      cycle6: '94',
-      exam2: '96',
-      sem2: '99'
-    }];
+  var client = new parentConnectionClient();
+  client.getSessionID(function(){
+    client.login(req.body.username, req.body.password, function(){
+      client.getGrades(function(){
+        client.logout(function(){
+          res.render('grades', { grades: client.grades });
+        });
+      });
+    });
+  });
 
-  res.render('grades', { grades: grades });
+
+//  var grades = [{
+//    teacher: 'Copeland, Joshua',
+//  course: 'US Hist AP D E',
+//  period: '01',
+//  cycle1: '97',
+//  cycle2: '93',
+//  cycle3: '95',
+//  exam1: '74',
+//  sem1: '91',
+//  cycle4: '92',
+//  cycle5: '93',
+//  cycle6: '94',
+//  exam2: '96',
+//  sem2: '99'
+//},
+//    {
+//      teacher: 'Copeland, Joshua',
+//      course: 'US Hist AP D E',
+//      period: '01',
+//      cycle1: '97',
+//      cycle2: '93',
+//      cycle3: '95',
+//      exam1: '74',
+//      sem1: '91',
+//      cycle4: '92',
+//      cycle5: '93',
+//      cycle6: '94',
+//      exam2: '96',
+//      sem2: '99'
+//    }];
+//
+//  res.render('grades', { grades: grades });
 
 };
